61731f345a49f5e4440ca873d837763a6f57b063
Pablo Firulo <pfirulo@yahoo.com>
2024-03-27 22:22:15 -0300

    funcionando variables env

--- server/database/database.go
+++ server/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"log"
+	"os"
 
 	"github.com/pfirulo2022/go-blog/model"
 	"gorm.io/driver/mysql"
@@ -13,7 +14,11 @@ var DBConn *gorm.DB
 
 func ConnectDB() {
 
-	dsn := "root:Agrale@tcp(127.0.0.1:3306)/fiber_blog?charset=utf8mb4&parseTime=True&loc=Local"
+	user := os.Getenv("db_user")
+	password := os.Getenv("db_password")
+	dbname := os.Getenv("db_name")
+
+	dsn := user + ":" + password + "@tcp(127.0.0.1:3306)/" + dbname + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Error),
 	})
--- server/server.go
+++ server/server.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
 	"github.com/pfirulo2022/go-blog/database"
 	"github.com/pfirulo2022/go-blog/router"
 )
 
 func init() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	database.ConnectDB() // Connect to the database
 }
 
